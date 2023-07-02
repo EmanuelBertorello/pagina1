a5b5942076e4c831b55918ebc0188cf0650bab9e
ema <ema-ber2011@live.com.ar>
2023-07-02 15:57:21 -0300

    cansado

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,18 +7,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerUsuario: FormGroup
-  constructor(private fb: FormBuilder){
-   this.registerUsuario = this.fb.group ({
-     email: ["",Validators.required],
-     password: ["",Validators.required],
-     repetirPassword: ["",Validators.required]
+   registerUsuario: FormGroup;
 
- 
+  constructor(private fb: FormBuilder){
+   this.registerUsuario = this.fb.group({
+     email: ['', Validators.required],
+     password: ['', Validators.required],
+     repetirPassword: ['', Validators.required]
    })
   }
    ngOnInit(): void {
      
    }
- }
+   registrar() {
+      console.log(this.registerUsuario);
+   }
+  }
  
